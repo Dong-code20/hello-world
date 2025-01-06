f6ab24790daa5c3f307291513eab4fff08f18624
Dong-code20 <2608450737@qq.com>
2025-01-06 23:41:10 +0800

    feat: 组件使用

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,160 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<el-container>
+		<el-herder
+			height="80px"
+			style="padding: 0"
+		>
+			<div class="header">教务系统学生管理</div>
+		</el-herder>
+		<el-container>
+			<el-aside width="200px">
+				<el-menu
+					default-active="1"
+					class="aside"
+					@select="selectFunc"
+					:unique-opened="true"
+				>
+					<el-submenu index="1">
+						<template #title>
+							<span>七年级</span>
+						</template>
+						<el-menu-item index="1">1班</el-menu-item>
+						<el-menu-item index="2">2班</el-menu-item>
+						<el-menu-item index="3">3班</el-menu-item>
+					</el-submenu>
+					<el-submenu index="2"
+						><template #title>
+							<span>八年级</span>
+						</template>
+						<el-menu-item index="4">4班</el-menu-item>
+						<el-menu-item index="5">5班</el-menu-item>
+						<el-menu-item index="6">6班</el-menu-item>
+					</el-submenu>
+					<el-submenu index="3">
+						<template #title>
+							<span>九年级</span>
+						</template>
+						<el-menu-item index="7">7班</el-menu-item>
+						<el-menu-item index="8">8班</el-menu-item>
+						<el-menu-item index="9">9班</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			</el-aside>
+			<el-container>
+				<el-header
+					height="80px"
+					style="padding: 0; margin: 0"
+				>
+					<el-container class="subHeader">
+						<div class="desc">{{ desc }}</div>
+						<el-button
+							style="width: 100px; height: 30px; margin: 20px"
+							>新增记录</el-button
+						>
+					</el-container>
+				</el-header>
+				<el-main style="margin: 0; padding: 0">
+					<div class="content">
+						<el-table :data="stus">
+							<el-table-column
+								prop="name"
+								label="姓名"
+							>
+							</el-table-column>
+							<el-table-column
+								prop="age"
+								label="年龄"
+							></el-table-column>
+							<el-table-column
+								prop="sex"
+								label="性别"
+							></el-table-column>
+							<el-table-column
+								prop="date"
+								label="录入日期"
+							></el-table-column>
+						</el-table>
+					</div>
+				</el-main>
+				<el-footer
+					height="30px"
+					class="footer"
+					>Vue框架搭建，ElementPlus提供组件支持</el-footer
+				>
+			</el-container>
+		</el-container>
+	</el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	data() {
+		return {
+			desc: "七年级1班学生统计",
+			stus: [
+				{
+					name: "小王",
+					age: 14,
+					sex: "男",
+					date: "2020年8月15日",
+				},
+				{
+					name: "小李",
+					age: 15,
+					sex: "女",
+					date: "2020年8月15日",
+				},
+				{
+					name: "小张",
+					age: 16,
+					sex: "男",
+					date: "2020年8月15日",
+				},
+			],
+		};
+	},
+	methods: {
+		selectFunc(index) {
+			let strs = ["七", "八", "九"];
+			let rank = strs[Math.floor((index - 1) / 3)];
+			this.desc = `${rank}年级${((index - 1) % 3) + 1}班学生统计`;
+		},
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+.header {
+	font-size: 30px;
+	line-height: 80px;
+	background-color: #f1f1f1;
+}
+.aside {
+	background-color: wheat;
+	height: 600px;
+}
+.subHeader {
+	background-color: cornflowerblue;
+}
+.desc {
+	font-size: 25px;
+	line-height: 80px;
+	color: white;
+	width: 800px;
+}
+.footer {
+	background-color: dimgray;
+	color: white;
+	font-size: 17px;
+	line-height: 30px;
 }
 </style>
